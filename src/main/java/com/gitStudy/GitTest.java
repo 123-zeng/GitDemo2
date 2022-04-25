4cdf06dac09cbd3a41b28b3c7bdb496a24fdaa65
zll <m2019zll@163.com>
2022-04-25 10:14:11 +0800

    first commit!

---
+++ src/main/java/com/gitStudy/GitTest.java
@@ -0,0 +1,13 @@
+package com.gitStudy;
+/*
+ * @author:  Liling Zeng
+ * @date:  2022/4/25 10:10
+ * @Email:m2019zll@163.com
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
